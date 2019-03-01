b7b2f32f98ba4d1ff4267a4b108a6e9a1ca9a454
Bryan Herbert <bryan77@twc.com>
2019-03-01 11:51:10 +0000

    added search ability

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ var app = express();
 var request = require('request');
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res) {
+    res.render("search");
+});
+
 app.get("/results", function(req, res) {
-    request("http://www.omdbapi.com?s=Indiana&apikey=thewdb", function(error, response, body) {
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com?s=" + query + "&apikey=thewdb";
+    request(url, function(error, response, body) {
         if (!error && response.statusCode == 200) {
             var data = JSON.parse(body);
             res.render("results", {data: data});
         }
     });
 });
-
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Movie App has started!");
